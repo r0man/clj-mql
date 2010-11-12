817f78000a2fb535c24238da53414c3cd991215c
Roman Scherer <roman@burningswell.com>
2010-11-12 20:42:46 +0100

    Updated to clojure v1.2.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject com.twinql.clojure/mql
-  "1.0.7"
+(defproject com.twinql.clojure/mql "1.0.7"
   :description "Freebase MQL client library."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [com.twinql.clojure/clj-apache-http "2.1.0"]])
 
